02c7eb7292816ef5d7a3df8c1420de10e9399f09
Eric Wohnlich <oolitegroove@gmail.com>
2014-08-21 20:12:16 -0400

    make fields not required

--- models.py
+++ models.py
@@ -12,7 +12,7 @@ socket_choices=[('red','Red'),
 # Create your models here.
 class GearItem(models.Model):
     name = models.CharField(max_length=80)
-    nameDescription = models.CharField(max_length=80)
+    nameDescription = models.CharField(max_length=80,blank=True)
     zone = models.CharField(max_length=80)
     source = models.CharField(max_length=80)
     agility = models.IntegerField(default=0)
@@ -25,13 +25,13 @@ class GearItem(models.Model):
     icon = models.CharField(max_length=80)
     slot = models.IntegerField(default=0)
     quality = models.IntegerField(default=0)
-    socket1 = models.CharField(max_length=20,
+    socket1 = models.CharField(max_length=20,blank=True,
                                choices=socket_choices,)
-    socket2 = models.CharField(max_length=20,
+    socket2 = models.CharField(max_length=20,blank=True,
                                choices=socket_choices,)
-    socket3 = models.CharField(max_length=20,
+    socket3 = models.CharField(max_length=20,blank=True,
                                choices=socket_choices,)
-    socket_bonus = models.CharField(max_length=80)
+    socket_bonus = models.CharField(max_length=80,blank=True)
     
     def slotName(self):
       return slots[self.slot]
